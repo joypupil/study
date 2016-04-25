f87144ec3db54bb938d307df299c1632f71a1453
hezikang <hezikang@hezikang-PC>
2016-04-25 15:17:21 +0800

    增加建造者模式

---
+++ study/src/main/java/com/joypupil/study/design_pattern/builder/Director.java
@@ -0,0 +1,22 @@
+package com.joypupil.study.design_pattern.builder;
+
+/**
+ * 导演类</br>
+ * 导演类起到封装的作用，避免高层模块深入到建造者内部的实现类
+ * @author hezikang
+ *
+ */
+public class Director {
+	
+	private Builder builder = new ConcreteProduct();
+	
+	//构建不同的产品
+	public Product getAProduct(){
+		builder.setPart();
+		/*
+		 * 设置不同的零件，产生不同的产品
+		 */
+		return builder.buildProduct();
+	}
+
+}
